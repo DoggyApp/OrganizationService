435c35e7dac06a957249f6e7ee08fb6d73aebaa4
Ian Martin <idanzigm@gmail.com>
2026-05-01 16:10:51 -0400

    removed duplicate beans

--- src/main/java/com/example/OrganizationService/OrganizationServiceApplication.java
+++ src/main/java/com/example/OrganizationService/OrganizationServiceApplication.java
@@ -3,20 +3,13 @@ package com.example.OrganizationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrganizationServiceApplication.class, args);
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
